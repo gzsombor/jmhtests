02d686ff26102f17d4b514ff9c70d629aecfd3d5
Zsombor Gegesy <gzsombor@gmail.com>
2021-10-24 00:05:53 +0200

    Use blackhole explicitly, set collect=1 and iterations=100000

--- src/main/java/jmhtests/StringWriterBenchmark.java
+++ src/main/java/jmhtests/StringWriterBenchmark.java
@@ -13,25 +13,27 @@ import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
 
 @State(Scope.Benchmark)
 public class StringWriterBenchmark {
 
-    private final static int iterations = 10000;
+    private final static int iterations = 100000;
 
-    @Param({ "100", "1000", "10000" })
-    private int collect = 100;
+    @Param({ "1" })
+    private int collect = 1;
 
     @Benchmark
     @Fork(value = 1, warmups = 1)
     @BenchmarkMode(Mode.Throughput)
-    public int stringWriter() throws IOException {
+    public int stringWriter(Blackhole blackhole) throws IOException {
         StringWriter sw = new StringWriter();
         generate(sw);
         int sum = 0;
         for (int i = 0; i < collect; i++) {
             var result = sw.toString();
             sum += result.length();
+            blackhole.consume(result);
         }
         return sum;
     }
@@ -39,14 +41,15 @@ public class StringWriterBenchmark {
     @Benchmark
     @Fork(value = 1, warmups = 1)
     @BenchmarkMode(Mode.Throughput)
-    public int byteArray() throws IOException {
+    public int byteArray(Blackhole blackhole) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        OutputStreamWriter sw = new OutputStreamWriter(baos);
+        OutputStreamWriter sw = new OutputStreamWriter(baos, "UTF-8");
         generate(sw);
         int sum = 0;
         for (int i = 0; i < collect; i++) {
             var result = baos.toString("UTF-8");
             sum += result.length();
+            blackhole.consume(result);
         }
         return sum;
     }
